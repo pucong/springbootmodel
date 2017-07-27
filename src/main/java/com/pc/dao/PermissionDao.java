4e4a3f674f6416eb5ec64c6516e5bcfccffe31d0
pucong <906886111@qq.com>
2017-07-27 23:19:53 +0800

    123

---
+++ src/main/java/com/pc/dao/PermissionDao.java
@@ -0,0 +1,13 @@
+package com.pc.dao;
+
+import com.pc.model.Permission;
+
+import java.util.List;
+
+/**
+ * Created by PC on 2017/7/27.
+ */
+public interface  PermissionDao {
+    public List<Permission> findAll();
+    public List<Permission> findByAdminUserId(int userId);
+}
---
+++ src/main/java/com/pc/model/SysUser.java
@@ -0,0 +1,34 @@
+package com.pc.model;
+
+/**
+ * Created by PC on 2017/7/27.
+ */
+public class SysUser {
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
